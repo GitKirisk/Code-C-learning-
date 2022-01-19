524d2987485ad00666cab7db0cea6cc4d5054c96
GitKirisk <97832148+GitKirisk@users.noreply.github.com>
2022-01-19 20:24:11 +0800

    Find prime numbers within 100
    Find primes within 100

---
+++ 22_1_19.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	printf("100以内的素数有：\n");
+	int dividend, divisor;
+	for (dividend = 2; dividend <= 100; dividend++)
+	{
+		for (divisor = 2; divisor <= 100; divisor++)
+		{
+			if (dividend % divisor == 0)
+				break;
+		}
+		if (divisor >= dividend)
+     		printf("%d ", dividend);
+	}
+	return 0;
+}
+//求100以内的素数
